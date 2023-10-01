0461da3201f4ef6cceafc2e191d252e0170d744a
Joel S. Hernández <joel.hernandez-c@mail.escuelaing.edu.co>
2023-10-01 14:11:21 -0500

    updated main.cpp

--- eje15_Estacion/main.cpp
+++ eje15_Estacion/main.cpp
@@ -11,19 +11,21 @@ void delay( unsigned int millisec ){
 
 using namespace std;
 
-int main(int argc, char** argv){
+int main(int argc, char **argv)
+{
     Medicion med, prom;
-    DB_Local db("dblocal.als");
+    DB_Local db;
     Sensor   sensor;
     
     while( true ){
         delay( 5000 );
         sensor.actualizar( med );
         db.Abrir_DB();
-        db.Ingresar( med );
-        db.Recuperar( prom );
-        cout << "Promedio: "  << prom << endl;
+        db.Ingresar(med);
+        db.Recuperar(prom);
+        cout << "Promedio: " << prom << endl;
         db.Cerar_DB();
-     }
+    }
     return 0;
 }
+
